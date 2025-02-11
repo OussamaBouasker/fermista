56318ad64f61110774c35fd29f09a17de0a58fa5
yosrjemli <yosrjemli05@gmail.com>
2025-02-11 04:36:47 +0100

    integration crud with back-office :white_check_mark:

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,59 +10,84 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 #[Route('/user')]
 final class UserController extends AbstractController
 {
     #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('Back/user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Get password from form
+            $plainPassword = $form->get('password')->getData();
+
+            if (!empty($plainPassword)) {
+                // Hash the password
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            }
+
+            $selectedRole = $form->get('roles')->getData(); // Get the selected role
+            $user->setRoles([$selectedRole]); // Store it as an array
+    
             $entityManager->persist($user);
             $entityManager->flush();
-
+    
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->render('user/new.html.twig', [
+    
+        return $this->render('Back/user/new.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
-        return $this->render('user/show.html.twig', [
+        return $this->render('Back/user/show.html.twig', [
             'user' => $user,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            // Get the new password from the form
+            $plainPassword = $form->get('password')->getData();
+    
+            // Check if the password field is not empty
+            if (!empty($plainPassword)) {
+                // Hash and set the new password
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+                
+            }
+            $selectedRole = $form->get('roles')->getData(); // Get the selected role
+            $user->setRoles([$selectedRole]); // Store it as an array
+    
             $entityManager->flush();
-
+    
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->render('user/edit.html.twig', [
+    
+        return $this->render('Back/user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -1,21 +1,44 @@
 <?php
 
 namespace App\Form;
-
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Enum\EnumRole;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+        ->add('email', TextType::class)
+        ->add('password', PasswordType::class, [
+            'mapped' => false, // This prevents Symfony from trying to set it directly on the entity
+            'required' =>false,
+        ])
+        ->add('roles', ChoiceType::class, [
+            'label' => 'Choisissez votre rôle',
+            'choices' => [
+                'Administrateur' => 'ROLE_ADMIN',
+                'Agriculteur' => 'ROLE_AGRICULTOR',
+                'Vétérinaire' => 'ROLE_VETERINAIR',
+                'Client' => 'ROLE_CLIENT',
+                'Formateur' => 'ROLE_FORMATEUR',
+            ],
+            'expanded' => false, // Dropdown menu
+            'multiple' => false, // Ensure single selection
+            'required' => true, // Mandatory field
+            'mapped'   => false, // Prevent Symfony from directly setting it
+            'attr' => [
+                'class' => 'block w-full p-3 border border-gray-300 rounded-md text-gray-900 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
+            ]
+            ]);
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
