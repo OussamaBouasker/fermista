abb0110e25fe6f8c22a5c31ebcd682ffe59569fd
OussamaBouasker <oussamabouasker6@gmail.com>
2025-02-11 03:39:57 +0100

    Crud evenement

--- migrations/Version20250211022829.php
+++ migrations/Version20250211022829.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250210210054 extends AbstractMigration
+final class Version20250211022829 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,6 +21,7 @@ final class Version20250210210054 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE collier (id INT AUTO_INCREMENT NOT NULL, vache_id INT DEFAULT NULL, reference VARCHAR(255) NOT NULL, taille VARCHAR(255) NOT NULL, valeur_temperature VARCHAR(255) NOT NULL, valeur_agitation VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1225992390065A44 (vache_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE vache (id INT AUTO_INCREMENT NOT NULL, age INT NOT NULL, race VARCHAR(255) NOT NULL, etat_medical VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE collier ADD CONSTRAINT FK_1225992390065A44 FOREIGN KEY (vache_id) REFERENCES vache (id)');
     }
 
@@ -29,5 +30,6 @@ final class Version20250210210054 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE collier DROP FOREIGN KEY FK_1225992390065A44');
         $this->addSql('DROP TABLE collier');
+        $this->addSql('DROP TABLE vache');
     }
 }
