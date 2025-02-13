c4188df0625caafb8a3485988ee997ce19e893d2
OussamaBouasker <oussamabouasker6@gmail.com>
2025-02-13 16:48:38 +0100

    update template front office + controlleur

--- migrations/Version20250213154443.php
+++ migrations/Version20250213154443.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250213153200 extends AbstractMigration
+final class Version20250213154443 extends AbstractMigration
 {
     public function getDescription(): string
     {
